843d43ff7bf01e693a4d9e6259e894a21f92a478
10_163_23_249\Administrator <1004695936@qq.com>
2017-12-21 18:01:31 +0800

    安装mysql模块

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
+// 引入mysql
+var mysql      = require('mysql');
+// mysql连接配置
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'wz198925',
+    database:'zw', // 前面建的user表位于这个数据库中
+    port: 3306
+});
+
+var testData = '';
+connection.connect();
+
+connection.query('SELECT * FROM user WHERE id =1', function(err,result) {
+    if (err) throw err;
+    console.log('The solution is: ', result);
+    testData = result
+});
+connection.end();
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    // res.render('index', { title: 'Express' });
+    res.json(testData);
 });
 
 module.exports = router;
